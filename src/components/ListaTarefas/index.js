ef7ced73166b3c7254afdbb4f714a9be670c15dd
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2022-11-28 21:22:51 -0300

    aula de renderização de listas

--- src/components/ListaTarefas/index.js
+++ src/components/ListaTarefas/index.js
@@ -12,15 +12,52 @@ import bin from "../../assets/bin.png";
 
 export function ListaTarefas() {
   const [novaTarefa, setNovaTarefa] = useState("");
-
+  const [lista, setLista] = useState(["Estudar", "Dormir", "Jantar"])
 
   const onChangeTarefa = (event) => {
     setNovaTarefa(event.target.value);
   };
 
-  const adicionaTarefa = () => {};
 
-  const removeTarefa = () => {};
+
+  const adicionaTarefa = () => { 
+    const novaLista = [...lista, novaTarefa];
+    setLista(novaLista)
+    setNovaTarefa("")
+  }
+
+
+
+  const removeTarefa = (tarefas) => { 
+    const novaLista = lista.filter((tarefa) => {
+      if (tarefas !== tarefa){
+      return tarefa
+    }
+    })
+    setLista(novaLista)
+  };
+
+  const tarefas = lista.map((tarefa, indice) => {
+    return (
+      <ListaContainer key={indice}>
+        <ul>
+          <Tarefa>
+            <p>{tarefa}</p>
+            <RemoveButton onClick={()=> removeTarefa(tarefa)}>
+              <img src={bin} alt="" width="16px" />
+            </RemoveButton>
+          </Tarefa>
+        </ul>
+
+      </ListaContainer>
+
+      // <ul key={tarefa}>
+      //   <li>
+      //     {tarefa}
+      //   </li>
+      // </ul>
+    )
+  })
 
   return (
     <ListaTarefasContainer>
@@ -30,19 +67,11 @@ export function ListaTarefas() {
           value={novaTarefa}
           onChange={onChangeTarefa}
         />
-        <AddTaskButton>Adicionar</AddTaskButton>
+        <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
       </InputContainer>
+      {tarefas}
 
-      <ListaContainer>
-        <ul>
-          <Tarefa>
-            <p>Nova tarefa</p>
-            <RemoveButton>
-              <img src={bin} alt="" width="16px" />
-            </RemoveButton>
-          </Tarefa>
-        </ul>
-      </ListaContainer>
+
     </ListaTarefasContainer>
   );
 }
